8f893e77ea2604a32037a3577e09d67e0cc903a9
mahrdanialahsan <mahrdanial@gmail.com>
2026-01-25 02:06:54 +0500

    Rename PM2 config to .cjs for ES module compatibility and add PM2 npm scripts

---
+++ ecosystem.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  apps: [
+    {
+      name: "zayan-backend",
+      script: "./index.js",
+      interpreter: "node",
+      instances: 1,
+      exec_mode: "fork",
+      env: {
+        NODE_ENV: "production", // production or development
+        PORT: 5000,
+      },
+      env_file: ".env",
+      error_file: "./logs/pm2-error.log",
+      out_file: "./logs/pm2-out.log",
+      log_file: "./logs/pm2-combined.log",
+      time: true,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      merge_logs: true,
+      min_uptime: "10s",
+      max_restarts: 10,
+      restart_delay: 4000,
+    },
+  ],
+};
